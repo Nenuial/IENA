0190d765df32ab9ce3270b15f2fd02d136e20f1f
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-11-06 15:52:25 +0100

    Initial commit

---
+++ R/fetch.R
@@ -0,0 +1,9 @@
+library(googledrive)
+
+options(
+  gargle_oauth_cache = ".secrets",
+  gargle_oauth_email = TRUE
+)
+
+drive_ls("Concours/IENA") |> 
+  purrr::pwalk(~drive_download(..2, path = paste0("data/", ..1), overwrite = T))
