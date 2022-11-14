fcf96aeebf33c0671b41e76348a8077aa7578e3d
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-11-14 14:30:00 +0100

    Token in user folder

--- R/fetch.R
+++ R/fetch.R
@@ -1,10 +1,12 @@
 library(googledrive)
 
 options(
-  gargle_oauth_cache = ".secrets",
+  gargle_oauth_cache = "~/.secrets",
   gargle_oauth_email = "pascal.burkhard@gmail.com",
   gargle_verbosity = "debug"
 )
+
+drive_auth(cache = "~/.secrets")
 
 drive_ls("Concours/IENA") |> 
   purrr::pwalk(~drive_download(..2, path = paste0("data/", ..1), overwrite = T))
